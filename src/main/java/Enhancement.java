779b65e0410fe7709c102628c28741a9ba624705
SpitaleA <agustin.spitale@nybblegroup.com>
2022-10-05 15:51:00 -0300

    Algunos cambios a las clases y agrego Pasos

--- src/main/java/Enhancement.java
+++ src/main/java/Enhancement.java
@@ -1,8 +1,10 @@
+import java.util.List;
+
 public class Enhancement extends ReportType {
     private String sugerenciaResultados;
 
-    public Enhancement(String sugerenciaResultados) {
-        super();
+    public Enhancement(String titulo,String descripcion,String actualResult,String sugerenciaResultados) {
+        super(titulo,descripcion,actualResult);
         this.sugerenciaResultados = sugerenciaResultados;
     }
 
@@ -12,5 +14,12 @@ public class Enhancement extends ReportType {
 
     public void setSugerenciaResultados(String sugerenciaResultados) {
         this.sugerenciaResultados = sugerenciaResultados;
+    }
+
+    @Override
+    public String toString() {
+        return "Enhancement{" +
+                "sugerenciaResultados='" + sugerenciaResultados + '\'' +
+                "} \n " + super.toString();
     }
 }
--- src/main/java/ReportType.java
+++ src/main/java/ReportType.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class ReportType {
 
     private String titulo;
     private String descripcion;
     private int prioridad;
     private  int severidad;
-    //private List<Paso> pasos;
+    private List<Paso> pasos;
+    private String actualReuslt;
 
-
+    public ReportType(String titulo, String descripcion,String actualReuslt) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.actualReuslt = actualReuslt;
+        this.pasos = new ArrayList<>();
+    }
 
     public String getTitulo() {
         return titulo;
@@ -38,6 +47,33 @@ public abstract class ReportType {
 
     public void setSeveridad(int severidad) {
         this.severidad = severidad;
+    }
+
+    public List<Paso> getPasos() {
+        return pasos;
+    }
+
+    public void addPaso(Paso paso) {
+        this.pasos.add(paso);
+    }
+
+    public String getActualReuslt() {
+        return actualReuslt;
+    }
+
+    public void setActualReuslt(String actualReuslt) {
+        this.actualReuslt = actualReuslt;
+    }
+
+    @Override
+    public String toString() {
+        return "ReportType{" +
+                "titulo='" + titulo + "\n" +
+                ", descripcion='" + descripcion + "\n" +
+                ", prioridad=" + prioridad + "\n" +
+                ", severidad=" + severidad + "\n" +
+                ", pasos=" + pasos.toString() +
+                '}';
     }
 }
   /*enum Prioridad{
