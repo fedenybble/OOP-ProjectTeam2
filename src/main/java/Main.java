a3285c55e113a18b11c11b01d52da44461850bc4
SpitaleA <agustin.spitale@nybblegroup.com>
2022-10-05 11:49:27 -0300

    Borro main

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Enhancement prueba = new Enhancement("Adicionar funciones nuevas");
-        prueba.setDescripcion("hola");
-        System.out.println(prueba.getDescripcion());
-        System.out.println(prueba.getSugerenciaResultados());
-    }
-}
