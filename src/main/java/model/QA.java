0fadf80e827f3e86280d271ec23c43219518d559
fedenybble <federico.padin@nybblegroup.com>
2022-10-06 12:09:52 -0300

    Agregando metodos a QA.java

--- src/main/java/model/QA.java
+++ src/main/java/model/QA.java
@@ -1,8 +1,10 @@
 package model;
 
+import interfaces.IQA;
+
 import java.util.List;
 
-public class QA extends Owner{
+public class QA extends Owner implements IQA {
 
     private List<Bug> bugsReported;
     private List<TestCase> testCasesDone;
@@ -26,5 +28,15 @@ public class QA extends Owner{
 
     public void setTestCasesDone(List<TestCase> testCasesDone) {
         this.testCasesDone = testCasesDone;
+    }
+
+    @Override
+    public void addBug(Bug bug) {
+        this.bugsReported.add(bug);
+    }
+
+    @Override
+    public void addTestCaseDone(TestCase testCase) {
+        this.testCasesDone.add(testCase);
     }
 }
