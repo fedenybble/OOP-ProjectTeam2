2616bedcbdaaf6c12e81abe6c064c88fd9227a50
fedenybble <federico.padin@nybblegroup.com>
2022-10-06 11:29:34 -0300

    Se creo la clase Owner.java y se elimino la clase Person.java. QA y DEV heredan de Owner.

--- src/main/java/model/Owner.java
+++ src/main/java/model/Owner.java
@@ -1,37 +1,83 @@
 package model;
 
-public class Owner {
-    private String name;
-    private String lastName;
-    private String role;
+    public class Owner {
 
-    public Owner(String name, String lastName, String role) {
-        this.name = name;
-        this.lastName = lastName;
-        this.role = role;
+        private String name;
+        private String lastname;
+
+        private String role;
+        private String email;
+        private String address;
+        private String dni;
+        private int phoneNumber;
+
+        public Owner(String name, String lastname, String role, String email, String address, String dni, int phoneNumber) {
+            this.name = name;
+            this.lastname = lastname;
+            this.role = role;
+            this.email = email;
+            this.address = address;
+            this.dni = dni;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getDni() {
+            return dni;
+        }
+
+        public void setDni(String dni) {
+            this.dni = dni;
+        }
+
+        public int getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(int phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-}
